086c6aafd242e1cdef86bf65ccba3b19a5ba0f37
Jie Yang <jieynlp@gmail.com>
2019-04-01 13:10:36 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# TAHV：Text Attention Heatmap Visualization
+
+This repository provides a simple visualization tool for the attention based NLP tasks. 
+
+Many attention based NLP tasks visualize the text with attention weights as background. This code takes word list and the corresponding weights as input and generate the Latex code to visualize the attention based text. The Latex code will generates a standalone `.pdf` visulization file. Users can use this `.pdf` visulization file as vector diagram to demonstrate the attention ability of the model in their papers/slides/demos.
+
+
+##Usage
+It is very simple to use this code. Feed the word list and weight list in function `generate` with output Latex file directory and color configuration. The Latex file will be generated. Then compile the Latex file, the `.pdf` file will be generated.
+
+
+
+![alt text](red.pdf "Red demo")
+![alt text](blue.pdf "Blue demo")
+
+
+* 2019-Apr-01, init version
+
---
+++ text_attention.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# @Author: Jie Yang
+# @Date:   2019-03-29 16:10:23
+# @Last Modified by:   Jie Yang,     Contact: jieynlp@gmail.com
+# @Last Modified time: 2019-04-01 13:06:55
+
+
+## convert the text/attention list to latex code, which will further generates the text heatmap based on attention weights.
+import numpy as np
+
+latex_special_token = ["!@#$%^&*()"]
+
+def generate(text_list, attention_list, latex_file, color='red'):
+	assert(len(text_list) == len(attention_list))
+	attention_list = rescale(attention_list)
+	word_num = len(text_list)
+	with open(latex_file,'w') as f:
+		f.write(r'''\documentclass[varwidth]{standalone}
+\special{papersize=210mm,297mm}
+\usepackage{color}
+\usepackage{tcolorbox}
+\usepackage{adjustbox}
+\tcbset{width=0.9\textwidth,boxrule=0pt,colback=red,arc=0pt,auto outer arc,left=0pt,right=0pt,boxsep=5pt}
+\begin{document}'''+'\n')
+		string = r'''{\setlength{\fboxsep}{0pt}\colorbox{white!0}{\parbox{0.9\textwidth}{'''+"\n"
+		for idx in range(word_num):
+			string += "\\colorbox{%s!%s}{"%(color, attention_list[idx])+"\\strut " + text_list[idx]+"} "
+		string += "\n}}}"
+		f.write(string+'\n')
+		f.write(r'''\end{document}''')
+
+def rescale(input_list):
+    the_array = np.asarray(input_list)
+    the_max = np.max(the_array)
+    the_min = np.min(the_array)
+    rescale = (the_array - the_min)/(the_max-the_min)*100
+    return rescale.tolist()
+
+
+
+if __name__ == '__main__':
+	sent = '''the USS Ronald Reagan - an aircraft carrier docked in Japan - during his tour of the region, vowing to "defeat any attack and meet any use of conventional or nuclear weapons with an overwhelming and effective American response".
+North Korea and the US have ratcheted up tensions in recent weeks and the movement of the strike group had raised the question of a pre-emptive strike by the US.
+On Wednesday, Mr Pence described the country as the "most dangerous and urgent threat to peace and security" in the Asia-Pacific.'''
+	words = sent.split()
+	attention = list(range(len(words)))
+	color = 'red'
+	generate(words, attention, "sample.tex", color)
